29c89a72b747d3f6cff2c9a8e28ff0bb794881dc
Billy Keyes <commits@billykeyes.dev>
2026-01-24 11:30:49 -0500

    Disable too-many-locals in event handlers

--- ExportCutlist.py
+++ ExportCutlist.py
@@ -189,6 +189,8 @@ class CutlistCommandCreatedEventHandler(adsk.core.CommandCreatedEventHandler):
 
     @report_errors
     def notify(self, args):
+        # pylint: disable=too-many-locals
+
         app = adsk.core.Application.get()
         design = adsk.fusion.Design.cast(app.activeProduct)
 
@@ -274,6 +276,8 @@ class CutlistCommandExecuteHandler(adsk.core.CommandEventHandler):
 
     @report_errors
     def notify(self, args):
+        # pylint: disable=too-many-locals
+
         eventArgs = adsk.core.CommandEventArgs.cast(args)
         inputs = eventArgs.command.commandInputs
 
